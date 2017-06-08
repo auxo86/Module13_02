d6fd5caf3f5526c2f0172b7639d07f9e67a66900
auxo86 <auxo86@gmail.com>
2017-06-08 19:32:23 +0800

    <handlers>       <add name="Module13_02" type="Module13_02.CustomHandler"            path="*.xyz" verb="*" />     </handlers>
    也要註冊在<system.webServer>下面

---
+++ Module13_02/CustomHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+
+namespace Module13_02
+{
+    public class CustomHandler : IHttpHandler
+    {
+        /// <summary>
+        /// You will need to configure this handler in the Web.config file of your 
+        /// web and register it with IIS before being able to use it. For more information
+        /// see the following link: http://go.microsoft.com/?linkid=8101007
+        /// </summary>
+        #region IHttpHandler Members
+
+        public bool IsReusable
+        {
+            // Return false in case your Managed Handler cannot be reused for another request.
+            // Usually this would be false in case you have some state information preserved per request.
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            //write your handler implementation here.
+            context.Response.Write(DateTime.Now.ToString());
+        }
+
+        #endregion
+    }
+}
